858306b12d4f2ff2115ebadc0b4f65e913e4eee1
Gabriel Caetano <gabrielc0202@hotmail.com>
2024-06-25 21:46:43 -0300

    MIDDLEWARE

--- routes.ts
+++ routes.ts
@@ -1,13 +1,23 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import AlunoController from "./src/controllers/AlunoControllers";
 
 const router = Router();
 
 const alunoController = new AlunoController();
 
+const authMiddleware = (Req: Request, Res: Response , next: NextFunction) => {
+    //Executar as Verificações necessárias
+      if(Req.headers.authorization) {
+      // Validar o token para identificar a existencia do usúario
+      next();
+      }else{
+         return new Error('Usúario não autenticado!');
+      }
+  };
+
 router.get('/', );
 
-router.get('/aluno', alunoController.getAll); //Buscar todos alunos
+router.get('/aluno', authMiddleware, alunoController.getAll); //Buscar todos alunos
 router.get('/alunos/:id', alunoController.getById ); // Buscar por um Aluno
 router.post('/aluno', alunoController.add); //Adicionar um ALuno
 router.put('/aluno/:id', alunoController.update); //Editar aluno
